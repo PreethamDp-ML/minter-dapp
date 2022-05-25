7b849bfee2cc9b5eeda2e60e6fbfe7e03d86db58
PreethamDp-TS <87472413+PreethamDp-ML@users.noreply.github.com>
2022-05-25 16:55:56 +0530

    q

--- backend/src/config.js
+++ backend/src/config.js
@@ -62,7 +62,7 @@ const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2022-07-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-05-20T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xc1Dc3547c41D1C2f30EDEaF21Bb3F3B5bF512EF6"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
